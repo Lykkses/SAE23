b2651b6d0eb5ed0196928501b9b1d4b89ac89c02
Lykkses <thomas-strub@orange.fr>
2022-06-08 16:27:08 +0200

    Création des views

---
+++ Aeroport/aeroports/avions_views.py
@@ -0,0 +1,58 @@
+from django.shortcuts import render, HttpResponseRedirect
+from . forms import AvionsForm
+from . import models
+
+
+def formulaire(request):
+    if request.method == "POST":
+        form = AvionsForm(request)
+        return render(request, "basedonnees/avions/formulaire.html", {"form": form})
+    else:
+        form = AvionsForm()
+        id = ""
+        return render(request, "basedonnees/avions/formulaire.html", {"form": form, "id": id})
+
+
+def traitement(request):
+    lform = AvionsForm(request.POST)
+    if lform.is_valid():
+        avions = lform.save()
+
+        return HttpResponseRedirect('/avions/indexavions/')
+    else:
+        return render(request, "basedonnees/avions/formulaire.html", {"form": lform})
+
+
+def index(request):
+    liste = list(models.Avions.objects.all())
+    return render(request, 'basedonnees/avions/index.html', {'liste': liste})
+
+
+def affiche(request, id):
+    avions = models.Avions.objects.get(pk=id)
+    liste = models.Typeavions.objects.filter(avions_id=id)
+    liste2 = models.Compagnies.objects.filter(avions_id=id)
+    return render(request, 'basedonnees/avions/affiche.html', {"avions": avions, 'liste': liste, 'liste2': liste2})
+
+
+def update(request, id):
+    avions = models.Avions.objects.get(pk=id)
+    form = AvionsForm(avions.dico())
+    return render(request, 'basedonne/avions/formulaire.html', {'form': form, 'id': id})
+
+
+def updatetraitement(request, id):
+    lform = AvionsForm(request.POST)
+    if lform.is_valid():
+        avions = lform.save(commit=False)
+        avions.id = id
+        avions.save()
+        return HttpResponseRedirect('/aeroports/indexavions/')
+    else:
+        return render(request, "basedonnees/avions/formulaire.html", {"form": lform, "id": id})
+
+
+def delete(request, id):
+    avions = models.Avions.objects.get(pk=id)
+    avions.delete()
+    return HttpResponseRedirect('/aeroports/indexavions/')
--- Aeroport/aeroports/views.py
+++ Aeroport/aeroports/views.py
@@ -3,4 +3,55 @@ from . forms import AeroportsForm
 from . import models
 
 
+def formulaire(request):
+    if request.method == "POST":
+        form = AeroportsForm(request)
+        return render(request, "basedonnees/aeroport/formulaire.html", {"form": form})
+    else:
+        form = AeroportsForm()
+        id = ""
+        return render(request, "basedonnees/aeroport/formulaire.html", {"form": form, "id": id})
 
+
+def traitement(request):
+    lform = AeroportsForm(request.POST)
+    if lform.is_valid():
+        aeroport = lform.save()
+
+        return HttpResponseRedirect('/aeroport/indexaeroport/')
+    else:
+        return render(request, "basedonnees/aeroport/formulaire.html", {"form": lform})
+
+
+def index(request):
+    liste = list(models.Aeroports.objects.all())
+    return render(request, 'basedonnees/aeroport/index.html', {'liste': liste})
+
+
+def affiche(request, id):
+    aeroport = models.Aeroports.objects.get(pk=id)
+    liste = models.Aeroports.objects.filter(avions=id)
+    return render(request, 'basedonnees/aeroport/affiche.html', {"aeroport": aeroport, 'liste': liste})
+
+
+def update(request, id):
+    aeroport = models.Aeroports.objects.get(pk=id)
+    form = AeroportsForm(aeroport.dico())
+    return render(request, 'basedonne/aeroport/formulaire.html', {'form': form, 'id': id})
+
+
+def updatetraitement(request, id):
+    lform = AeroportsForm(request.POST)
+    if lform.is_valid():
+        aeroport = lform.save(commit=False)
+        aeroport.id = id
+        aeroport.save()
+        return HttpResponseRedirect('/aeroports/indexaeroport/')
+    else:
+        return render(request, "basedonnees/aeroport/formulaire.html", {"form": lform, "id": id})
+
+
+def delete(request, id):
+    aeroport = models.Aeroports.objects.get(pk=id)
+    aeroport.delete()
+    return HttpResponseRedirect('/aeroports/indexaeroport/')
